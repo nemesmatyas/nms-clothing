8526158e7550c4a819a4eed9026b59aebd84731b
nemesmatyas <nemesmatyas@gmail.com>
2020-03-08 10:31:55 +0100

    Redirect to homepage when user is signed in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user-actions';
@@ -48,7 +48,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={AccountPage} />
+          <Route exact path="/signin" render={() => this.props.currentUser ? <Redirect to="/" /> : <AccountPage />} />
         </Switch>
       </div>
     );
@@ -56,12 +56,17 @@ class App extends Component {
 }
 
 /**
- * Pass state to children components
- * Dispatch function takes the action, and passes it
+ * Dispatch the setCurrentUser action to the store
+ * Triggers state change
  */
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
