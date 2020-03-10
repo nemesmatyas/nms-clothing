5845e3434a6a2a5d2b381b521d04e41be792fe41
nemesmatyas <nemesmatyas@gmail.com>
2020-03-10 07:58:37 +0100

    Updated CartIcon and CartDropdown with Reselect to improve performance

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,18 +5,23 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { connect } from 'react-redux';
 import { toggleCartVisibility } from '../../redux/cart/cart-actions';
+import { selectCartItemsCount } from '../../redux/cart/cart-selectors';
 
-const CartIcon = ({ toggleCartVisibility }) => {
+const CartIcon = ({ toggleCartVisibility, itemCount }) => {
     return(
         <div className="cart-icon" onClick={toggleCartVisibility}>
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     )
 }
+
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartVisibility: () => dispatch(toggleCartVisibility())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart-selectors.js
@@ -0,0 +1,28 @@
+import { createSelector } from 'reselect';
+
+// Input selector. Gets the entire state and returns a slice of it
+const selectCart = state => state.cart;
+
+
+/**
+ * Output selector. Calls createSelector
+ * @param {array} selectCart - the array of input selectors needed
+ * @param {function} - takes the slices of state needed (cart, user, etc.) and returns the pieces we want to use as a memoized value
+ *  
+ */ 
+export const selectCartItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+);
+
+
+/**
+ * Output selector. Calls createSelector
+ * @param {array} selectCartItems - the array of input selectors needed
+ * @param {function} - takes the slices of state needed (cart, user, etc.) and returns a memoized calculated value.
+ * In this case, it is the overall quantity of cart items 
+ */ 
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    (cartItems) => cartItems.reduce(((accumulator, currentItem) => accumulator + currentItem.quantity), 0)
+)
