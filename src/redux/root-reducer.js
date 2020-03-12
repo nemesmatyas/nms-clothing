655b2b04279356f933f90a98617ebee619006fa2
nemesmatyas <nemesmatyas@gmail.com>
2020-03-12 08:55:19 +0100

    Added Redux Persist to store cart items

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,25 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // The built-in local storage on the Window object
 
 import userReducer from './user/user-reducer';
 import cartReducer from './cart/cart-reducer';
 
-export default combineReducers({
+/**
+ * Configuration for Redux Persist
+ * key: where the to start storing the data
+ * storage: what type of storage to use (session or local)
+ * whitelist: an array of reducers to store
+ */
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
